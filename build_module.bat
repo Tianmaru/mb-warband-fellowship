6cc58c042f51e5365932ad29670974c2733ac452
bh282 <bh282@uni-rostock.de>
2018-10-23 01:32:50 +0200

    initial commit

---
+++ build_module.bat
@@ -0,0 +1,36 @@
+@echo off
+"C:\Program Files\Python27\python.exe" process_init.py
+"C:\Program Files\Python27\python.exe" process_global_variables.py
+"C:\Program Files\Python27\python.exe" process_strings.py
+"C:\Program Files\Python27\python.exe" process_skills.py
+"C:\Program Files\Python27\python.exe" process_music.py
+"C:\Program Files\Python27\python.exe" process_animations.py
+"C:\Program Files\Python27\python.exe" process_meshes.py
+"C:\Program Files\Python27\python.exe" process_sounds.py
+"C:\Program Files\Python27\python.exe" process_skins.py
+"C:\Program Files\Python27\python.exe" process_map_icons.py
+"C:\Program Files\Python27\python.exe" process_factions.py
+"C:\Program Files\Python27\python.exe" process_items.py
+"C:\Program Files\Python27\python.exe" process_scenes.py
+"C:\Program Files\Python27\python.exe" process_troops.py
+"C:\Program Files\Python27\python.exe" process_particle_sys.py
+"C:\Program Files\Python27\python.exe" process_scene_props.py
+"C:\Program Files\Python27\python.exe" process_tableau_materials.py
+"C:\Program Files\Python27\python.exe" process_presentations.py
+"C:\Program Files\Python27\python.exe" process_party_tmps.py
+"C:\Program Files\Python27\python.exe" process_parties.py
+"C:\Program Files\Python27\python.exe" process_quests.py
+"C:\Program Files\Python27\python.exe" process_info_pages.py
+"C:\Program Files\Python27\python.exe" process_scripts.py
+"C:\Program Files\Python27\python.exe" process_mission_tmps.py
+"C:\Program Files\Python27\python.exe" process_game_menus.py
+"C:\Program Files\Python27\python.exe" process_simple_triggers.py
+"C:\Program Files\Python27\python.exe" process_dialogs.py
+"C:\Program Files\Python27\python.exe" process_global_variables_unused.py
+"C:\Program Files\Python27\python.exe" process_postfx.py
+echo.
+echo ______________________________
+echo.
+echo Script processing has ended.
+echo Press any key to exit. . .
+pause>nul
